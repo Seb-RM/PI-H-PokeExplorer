bc89a250fe48118b9063f65026653e0612bdbc98
Seb-RM <srmmoto@gmail.com>
2024-01-03 19:23:45 -0300

    Creación de la carpeta routes y el index para routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import server from "./src/App.js";
+import sequelize from "./src/DB_connection.js";
 
 
 const { PORT } = process.env
 
+sequelize.sync().then( () => {
 
-server.listen( PORT, ()=> {
+    server.listen( PORT, ()=> {
     console.log(`Servidor funcionando en puerto: ${PORT}`);
+    });
+
 });
