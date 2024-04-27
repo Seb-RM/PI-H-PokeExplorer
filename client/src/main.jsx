8fd596e05f02ecd0a6f3ee80fea3add6e8d86b87
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-04-26 23:02:57 -0300

    cambio en la url constante de axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index.js"
 
-axios.defaults.baseURL = "pi-h-pokeexplorer-production.up.railway.app";
+axios.defaults.baseURL = "https://pi-h-pokeexplorer-production.up.railway.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
