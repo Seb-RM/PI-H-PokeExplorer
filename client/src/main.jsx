b29c27e3bbd41579db95cde0c0a5df9f9c62347d
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-04-26 22:39:54 -0300

    se agrega la la constante de url de axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import axios from "axios";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,6 +7,9 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index.js"
+
+axios.defaults.baseURL =
+  "https://ricky-morty-appremake-backend-production.up.railway.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
