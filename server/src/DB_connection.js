408430c792d782ebb1931f5fe2602179c314d547
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-04-26 22:47:25 -0300

    reconfiguracion para la conexion con railway

--- server/src/DB_connection.js
+++ server/src/DB_connection.js
@@ -1,10 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_RAILWAYS_URL} = process.env;
+
+// const sequelize = new Sequelize(
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//     { logging: false }
+// );
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    { logging: false }
+    DB_RAILWAYS_URL,
+    { logging: false, native: false }
 );
 
 export default sequelize;
